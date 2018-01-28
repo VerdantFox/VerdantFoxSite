4f6bc72025fa2217ed13696b75468b90beaa3b9d
Teddy Williams <verdantfoxx@gmail.com>
2018-01-28 15:21:23 -0700

    Updated landing page with Projects section. Made carousels of pictures for each project. Continued updating visuals to work with new color scheme. Edited some app descriptions.

--- VerdantFox/settings.py
+++ VerdantFox/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'SECRET_KEY_GOES_HERE'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] # '192.168.0.104', 'localhost:8000',
 
 
 # Application definition
