5c52ceea60c0cb09b85301fbd228c08f860fab8a
Teddy Williams <verdantfoxx@gmail.com>
2018-03-09 07:51:11 -0700

    Fixed a bug for number of columns for "funds" and "net worth" in Finance, portfolio template.

--- Simple/tests.py
+++ Simple/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .views import change_machine
 
 # Create your tests here.
+class ChangeTests(TestCase):
+
+    def test_exact_change_given(self):
+        """exact_change returns True if exact change is given"""
+
+        item_cost = 10.52
+        amount_paid = 10.52
+
+
